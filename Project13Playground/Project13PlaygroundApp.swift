9a509f504d667cafcb64cb19fdde713edca58060
Henny2 <henrieke.baunack@web.de>
2023-12-26 16:03:49 -0800

    Initial Commit

---
+++ Project13Playground/Project13PlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project13PlaygroundApp.swift
+//  Project13Playground
+//
+//  Created by Henrieke Baunack on 12/26/23.
+//
+
+import SwiftUI
+
+@main
+struct Project13PlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
